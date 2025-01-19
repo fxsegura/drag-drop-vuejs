cf183998eb156282dc4fcd0f75af322e33aa4f17
francisco.segura <francisco.segura@jalasoft.com>
2025-01-19 14:32:16 -0500

    Added Table component for a main dashboard page.

---
+++ landing-page-builder/src/components/LandingPageTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <table class="w-full border-collapse border border-gray-300">
+    <thead>
+      <tr class="bg-gray-200">
+        <th class="border border-gray-300 p-2">Preview</th>
+        <th class="border border-gray-300 p-2">Name</th>
+        <th class="border border-gray-300 p-2">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <TableRow
+        v-for="page in pages"
+        :key="page.id"
+        :page="page"
+        @editPage="$emit('editPage', page.id)"
+        @deletePage="$emit('deletePage', page.id)"
+      />
+    </tbody>
+  </table>
+</template>
+
+<script>
+import TableRow from './LandingPageTableRow.vue'
+
+export default {
+  name: 'LandingPageTable',
+  props: {
+    pages: Array,
+  },
+  components: {
+    TableRow,
+  },
+}
+</script>
---
+++ landing-page-builder/src/components/LandingPageTableRow.vue
@@ -0,0 +1,31 @@
+<template>
+  <tr>
+    <td class="border border-gray-300 p-2">
+      <img :src="page.preview" alt="Preview" class="w-16 h-16 rounded" />
+    </td>
+    <td class="border border-gray-300 p-2">{{ page.name }}</td>
+    <td class="border border-gray-300 p-2">
+      <button
+        @click="$emit('editPage')"
+        class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
+      >
+        Edit
+      </button>
+      <button
+        @click="$emit('deletePage')"
+        class="ml-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+      >
+        Delete
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'LandingPageTableRow',
+  props: {
+    page: Object,
+  },
+}
+</script>
