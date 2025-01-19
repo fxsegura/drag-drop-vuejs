35c9ffc6b354a8137c803a732181d1f34c98258e
francisco.segura <francisco.segura@jalasoft.com>
2025-01-19 15:00:13 -0500

    Added additional tailwind styling config

--- landing-page-builder/src/App.vue
+++ landing-page-builder/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <nav class="p-4 bg-gray-200">
-      <router-link to="/" class="mr-4">Dashboard</router-link>
-      <router-link to="/builder">Landing Page Builder</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
