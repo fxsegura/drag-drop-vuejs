aa698490fb09284c49e53714613f3197654e10c6
francisco.segura <francisco.segura@jalasoft.com>
2025-01-20 00:53:58 -0500

    Added a toolbar that will include my draggable elements to build the page

---
+++ landing-page-builder/src/components/BuilderToolbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="bg-[#363638] h-full w-16 p-4 space-y-4">
+    <VueDraggable
+      v-for="item in toolbarItems"
+      :key="item.id"
+      class="p-2 border-2 border-dashed rounded-lg cursor-move hover:shadow-lg flex items-center justify-center hover:bg-[#2D2D2E]"
+      @mousedown.prevent
+    >
+      <svg
+        v-if="item.type === 'text'"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8 text-white"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+        />
+      </svg>
+      <svg
+        v-if="item.type === 'image'"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8 text-white"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+        />
+      </svg>
+    </VueDraggable>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { VueDraggable } from 'vue-draggable-plus'
+
+const toolbarItems = ref([
+  { id: 1, type: 'text' },
+  { id: 2, type: 'image' },
+])
+</script>
+
+<style scoped>
+.cursor-move:hover {
+  cursor: move;
+}
+</style>
